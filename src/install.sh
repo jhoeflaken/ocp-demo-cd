e842568c19f69b4aed05e4a03121ebb142ea239c
jhoeflak <jacob.hoeflaken@axians.com>
2021-11-08 09:52:02 +0100

    Added install en uninstall scripts.

---
+++ src/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+oc apply -f ./projects/project-dev.yaml
+oc apply -f ./ocp-demo-dev.yaml
---
+++ src/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+oc delete Application ocp-demo-dev
+oc delete AppProject ocp-demo-dev
